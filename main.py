aa8367b2ade6cb92c7d61b2654260695e606aedc
dina <ziatdinovadina73@email.ru>
2025-10-07 10:14:27 +0300

    добавили алиасы, исправили gitignore

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
+    bot.send_message(message.chat.id, f"вы написали: {message.text}")
 
 if __name__ == '__main__':
     bot.infinity_polling()
